75a308826563a56e0a4a4b295c04d88f7c7b3d37
Alexis Vanier <alexis@amonoid.io>
2019-02-18 00:50:51 -0500

    String splitting and multiple codes work!

--- gorecptprint.go
+++ gorecptprint.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
+	"image/draw"
 	"strconv"
+	"strings"
 
 	"itkettle.org/avanier/gorecptprint/lib/extras"
 
@@ -36,43 +39,68 @@ func main() {
 
 	Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Volutpat lacus laoreet non curabitur gravida. Nulla pellentesque dignissim enim sit amet venenatis urna cursus eget. At in tellus integer feugiat scelerisque varius morbi enim nunc. A scelerisque purus semper eget duis at. Sed lectus vestibulum mattis ullamcorper. Scelerisque purus semper eget duis at. Ut porttitor leo a diam sollicitudin. Sit amet aliquam id diam maecenas ultricies. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Posuere lorem ipsum dolor sit amet. Arcu vitae elementum curabitur vitae nunc sed velit dignissim. In est ante in nibh mauris cursus mattis molestie. In ante metus dictum at tempor commodo. Eu augue ut lectus arcu bibendum at varius vel. Eu scelerisque felis imperdiet proin fermentum leo vel. Sagittis nisl rhoncus mattis rhoncus urna. Dui faucibus in ornare quam viverra orci sagittis eu volutpat. Elit duis tristique sollicitudin nibh sit amet commodo nulla.
 	`
-	// extras.PrintDummyGraphic(options)
-	// tf6.ExecuteHex([]byte{0x1b, 0x64, 0x02}, options) // Feed N lines
-	// tf6.PrintString(daString, options)
-	// tf6.ExecuteHex([]byte{0x1b, 0x64, 0x02}, options) // Feed N lines
-
+	// daString := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris sit amet massa vitae. Pellentesque habitant morbi tristique senectus et netus. Sed lectus vestibulum mattis ullamcorper velit. Vitae sapien pellentesque habitant morbi tristique senectus et. Ac turpis egestas integer eget aliquet nibh praesent tristique. Aliquet eget sit amet tellus cras adipiscing enim. Netus et malesuada fames ac. Eget sit amet tellus cras adipiscing enim. Elit eget gravida cum sociis natoque penatibus et magnis. Diam volutpat commodo sed egestas egestas. Diam quam nulla porttitor massa. Condimentum lacinia quis vel eros donec ac odio. Eget duis at tellus at urna condimentum. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Lectus proin nibh nisl condimentum id venenatis a condimentum vitae. Fames ac turpis egestas integer eget aliquet nibh. Faucibus pulvinar elementum integer enim neque."
 	stringArray := extras.SplitString(daString, 174)
 
 	scaleFactor := 2
+	symbPerLine := 2
+	offset := 16 // px @ 8px/mm
 
-	// Print 3 DMTX per line
+	// Print x DMTX per line
 
-	// var numGroups int
-	// if len(stringArray) % 3 {
-	// 	numGroups = (len(stringArray) / 3) + 1
-	// } else {
-	// 	numGroups = (len(stringArray) / 3)
-	// }
+	var numGroups int
+	if len(stringArray)%symbPerLine != 0 {
+		numGroups = (len(stringArray) / symbPerLine) + 1
+	} else {
+		numGroups = (len(stringArray) / symbPerLine)
+	}
 
-	for i := 0; i < len(stringArray); i++ {
-		fmt.Println("This is symbol", i+1)
-		oneCode := stringArray[i]
-		// fmt.Println(oneCode)
-		tf6.PrintString(oneCode, options)
-		dmtxCode, _ := datamatrix.Encode(oneCode)
-		dmtxCode, _ = barcode.Scale(dmtxCode, dmtxCode.Bounds().Max.X*scaleFactor, dmtxCode.Bounds().Max.Y*scaleFactor)
+	for grInd := 0; grInd < numGroups; grInd++ {
+		stringBatch := make([]string, symbPerLine)
+		var masterRectangle image.Rectangle
+		var masterImage draw.Image
+		var baseWidth int
 
-		pixels, width, height := getPixels(dmtxCode)
-		// pixels, _, _ := getPixels(dmtxCode)
+		for sInd := 0; (sInd < symbPerLine) && ((grInd*symbPerLine)+sInd < len(stringArray)); sInd++ {
+			stringBatch[sInd] = stringArray[(grInd*symbPerLine)+sInd]
+			oneBarcode, _ := datamatrix.Encode(stringBatch[sInd])
+			oneBarcode, _ = barcode.Scale(oneBarcode, oneBarcode.Bounds().Dx()*scaleFactor, oneBarcode.Bounds().Dy()*scaleFactor)
+			if sInd == 0 {
+				baseWidth = oneBarcode.Bounds().Dx()
+			}
+			sp := image.Point{(baseWidth * sInd) + (offset * sInd), 0}    // place image starting point with offset
+			dr := image.Rectangle{sp, sp.Add(oneBarcode.Bounds().Size())} // make a drawing rectangle for holding them pixels
+			if sInd == 0 {
+				fullWidth := (oneBarcode.Bounds().Dx() * symbPerLine) + (offset * symbPerLine)
+				masterRectangle = image.Rectangle{image.ZP, image.Point{fullWidth, oneBarcode.Bounds().Dy()}}
+				masterImage = image.NewRGBA(masterRectangle)
+				white := color.RGBA{255, 255, 255, 255}
+				draw.Draw(masterImage, masterImage.Bounds(), &image.Uniform{white}, image.ZP, draw.Src) // apply primer on the whole rectangle
+			}
+			draw.Draw(masterImage, dr, oneBarcode, image.ZP, draw.Src)
+		}
 
-		// fmt.Println(pixels)
-		// fmt.Printf("%2x\n", pixels)
-		// fmt.Println(width, height)
+		tf6.PrintString(strings.Join(stringBatch, ""), options)
+		pixels, width, height := getPixels(masterImage)
 
 		dmtxProps := tf6.GraphicProps{D: 2, W: int16(width / 8), H: int16(height / 8)}
 		tf6.PrintGraphic(dmtxProps, pixels, options)
 		tf6.ExecuteHex(cmdFeed, options)
 	}
+
+	// for i := 0; i < len(stringArray); i++ {
+	// 	oneCode := stringArray[i]
+
+	// 	tf6.PrintString(oneCode, options)
+	// 	dmtxCode, _ := datamatrix.Encode(oneCode)
+	// 	dmtxCode, _ = barcode.Scale(dmtxCode, dmtxCode.Bounds().Max.X*scaleFactor, dmtxCode.Bounds().Max.Y*scaleFactor)
+
+	// 	pixels, width, height := getPixels(dmtxCode)
+
+	// 	dmtxProps := tf6.GraphicProps{D: 2, W: int16(width / 8), H: int16(height / 8)}
+	// 	tf6.PrintGraphic(dmtxProps, pixels, options)
+	// 	tf6.ExecuteHex(cmdFeed, options)
+	// }
 
 	tf6.ExecuteHex(cmdCut, options)
 	// extras.ByeTune(options)
